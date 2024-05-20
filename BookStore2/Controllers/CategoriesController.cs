7071dbd4ba6b247e0c107460205703f85c204b32
ayahwheidi <ayahwheidi@gmail.com>
2024-05-20 18:06:09 +0300

    show categories

---
+++ BookStore2/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using BookStore2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+       
+        public IActionResult Index()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
